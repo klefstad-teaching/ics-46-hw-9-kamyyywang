e04b7867c3b24ca53836685d10b84a001a9462c7
Kamish Ziqing Wang <kamishwang@gmail.com>
2025-03-15 17:02:34 -0700

    Update dijkstras.cpp

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -25,8 +25,8 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     vector<int> path;
-    if (distances[destination] == INF) return path;
-    if (path.size() == 1 && path[0] != destination) return {};
+    if (distances[destination] == INF) return {};
+    
     for (int v = destination; v != -1; v = previous[v]) {
         path.push_back(v);
     }
